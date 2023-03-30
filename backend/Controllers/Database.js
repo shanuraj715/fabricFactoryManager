ab2c3cf9efb4410be9278fd56bf4b4f81d625d08
shanuraj715 <shanuraj715@gmail.com>
2023-03-30 17:37:58 +0530

    reset db username and password

--- backend/Controllers/Database.js
+++ backend/Controllers/Database.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 function dbConnect() {
-    const dbUrl = 'mongodb+srv://shanuraj715:07426807250@myalbum.iqhru.mongodb.net/rajfabrics?retryWrites=true&w=majority'
+    const dbUrl = `mongodb+srv://shanuraj715:${process.env.DB_PASSWORD}@myalbum.iqhru.mongodb.net/rajfabrics?retryWrites=true&w=majority`
+    console.log(dbUrl)
     mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Connected to database.")
